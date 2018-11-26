5564ac0b30d0b17ef2873f9c78f214b6ecc85af8
Srikanth <ksreekanthyadav@gmail.com>
2018-11-26 18:17:36 +0530

    Payment

--- app/src/main/java/cubex/mahesh/yoursaloon/LoginActivity.java
+++ app/src/main/java/cubex/mahesh/yoursaloon/LoginActivity.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +20,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +30,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,6 +52,8 @@ public class LoginActivity extends AppCompatActivity {
 
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mDatabaseReference;
+
+    String AccountType, AccountID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,6 +112,9 @@ public class LoginActivity extends AppCompatActivity {
                     mProgressDialog.setMessage("Checking Approval Status");
                     String type = mSharedpref.getString("user_type", "none");
                     String id = mAuth.getCurrentUser().getUid();
+
+                    AccountType = type;
+                    AccountID = id;
                     if (type.equalsIgnoreCase("saloon")) {
                         mProgressDialog.show();
                         mFirebaseDatabase.getReference("saloons").child(id).child("accepted").addValueEventListener(new ValueEventListener() {
@@ -111,7 +129,8 @@ public class LoginActivity extends AppCompatActivity {
                                 ad.setPositiveButton("Upload Receipt", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
-                                        dialogInterface.dismiss();
+                                      //  dialogInterface.dismiss();
+                                        captureReceipt();
                                     }
                                 });
                                 ad.setNegativeButton("Pay Now", new DialogInterface.OnClickListener() {
@@ -151,6 +170,8 @@ public class LoginActivity extends AppCompatActivity {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
                                         dialogInterface.dismiss();
+                                        captureReceipt();
+
                                     }
                                 });
                                 ad.setNegativeButton("Pay Now", new DialogInterface.OnClickListener() {
@@ -186,6 +207,8 @@ public class LoginActivity extends AppCompatActivity {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
                                         dialogInterface.dismiss();
+                                        captureReceipt();
+
                                     }
                                 });
                                 ad.setNegativeButton("Pay Now", new DialogInterface.OnClickListener() {
@@ -238,6 +261,115 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public void captureReceipt() {
+        AlertDialog.Builder ad =
+                new AlertDialog.Builder(LoginActivity.this);
+        ad.setTitle("Your Salon");
+        ad.setMessage("Please select Image source.");
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == AlertDialog.BUTTON_POSITIVE) {
+                    Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
+                    startActivityForResult(i, 123);
+                } else if (which == AlertDialog.BUTTON_NEGATIVE) {
+
+                    Intent i = new Intent();
+                    i.setAction(Intent.ACTION_GET_CONTENT);
+                    i.setType("image/*");
+                    startActivityForResult(i, 124);
+
+                } else if (which == AlertDialog.BUTTON_NEUTRAL) {
+                    dialog.cancel();
+                }
+            }
+        };
+        ad.setPositiveButton("Camera", listener);
+        ad.setNegativeButton("Gallery", listener);
+        ad.setNeutralButton("Cancel", listener);
+        ad.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            Bitmap bmp = (Bitmap) data.getExtras().get("data");
+            //cview.setImageBitmap(bmp);
+
+
+            try {
+                FileOutputStream fos = openFileOutput("receipt.png",
+                        Context.MODE_PRIVATE);
+                bmp.compress(Bitmap.CompressFormat.PNG,
+                        100, fos);
+                fos.flush();
+                fos.close();
+
+                //    profile_pic_avaiable = true;
+
+                uploadReceipt();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if (requestCode == 124 && resultCode == RESULT_OK) {
+            try {
+
+                Uri u = data.getData();
+                //   cview.setImageURI(u);
+                Bitmap bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), u);
+
+                FileOutputStream fos = openFileOutput("receipt.png",
+                        Context.MODE_PRIVATE);
+                bmp.compress(Bitmap.CompressFormat.PNG,
+                        100, fos);
+                fos.flush();
+                fos.close();
+                //  profile_pic_avaiable = true;
+
+                uploadReceipt();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
+
+    void uploadReceipt() {
+        mProgressDialog.show();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference ref = storage.getReference("Receipts/" + AccountType + "/" + AccountID);
+        try {
+            FileInputStream fis = openFileInput("receipt.png");
+            ref.child("receipt.png").
+                    putStream(fis).
+                    addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mProgressDialog.dismiss();
+                            String url = ref.getDownloadUrl().toString();
+
+                          /*  FirebaseDatabase dBase = FirebaseDatabase.getInstance();
+                            DatabaseReference ref =  dBase.getReference("/users");
+                            DatabaseReference child_ref = ref.child("/"+uid);
+                            child_ref.child("business_women_commercial_reg_pic").setValue(url);*/
+
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
