4c0a8582276607d9f85fb7d3255f534dbdba8467
Mahesh <mahash.thippala@rentokil-initial.com>
2018-12-11 19:43:44 +0530

    Updated code for displyaing locations on Map

--- app/src/main/java/cubex/mahesh/yoursaloon/DashboardActivity.java
+++ app/src/main/java/cubex/mahesh/yoursaloon/DashboardActivity.java
@@ -7,13 +7,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import cubex.mahesh.yoursaloon.pojos.SaloonPojo;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    static ArrayList<SaloonPojo> saloons_list ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        saloons_list = new ArrayList<>();
+
+        loadApprovedSaloons( );
 
         if (android.os.Build.VERSION.SDK_INT > 21) {
             getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.gradient1));
@@ -28,10 +47,163 @@ public class DashboardActivity extends AppCompatActivity {
 
     }
 
-  public   void loadMap(View v)
+
+    void loadApprovedSaloons( ){
+
+
+
+        DatabaseReference dRef = FirebaseDatabase.getInstance().getReference("saloons");
+        dRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                saloons_list.clear();
+
+                Iterable<DataSnapshot> itrbl = dataSnapshot.getChildren();
+                Iterator<DataSnapshot> itr = itrbl.iterator();
+                while (itr.hasNext()){
+
+                    DataSnapshot snap = itr.next();
+
+                    Iterable<DataSnapshot> childs_uid = snap.getChildren();
+
+                    Iterator<DataSnapshot> child = childs_uid.iterator();
+
+                    SaloonPojo pojo = new SaloonPojo();
+
+                    while (child.hasNext()){
+
+                        DataSnapshot ds = child.next();
+
+                        /*Toast.makeText(DashboardActivity.this,
+                                ds.getKey(),Toast.LENGTH_LONG).show();*/
+
+                        switch (ds.getKey()){
+                            case "accepted":
+                                pojo.setAccepted(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "bodycare":
+                                pojo.setBodycare(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "city":
+                                pojo.setCity(ds.getValue().toString());
+                                break;
+                            case "email":
+                                pojo.setEmail(ds.getValue().toString());
+                                break;
+                            case "eyebrows":
+                                pojo.setEyebrows(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "haircut":
+                                pojo.setHaircut(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "hairprotein":
+                                pojo.setHairprotein(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "hairstyle":
+                                pojo.setHairstyle(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "hairtreatment":
+                                pojo.setHairtreatment(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "hennadesign":
+                                pojo.setHennadesign(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "location":
+                                pojo.setLocation(ds.getValue().toString());
+                                break;
+                            case "makeup":
+                                pojo.setMakeup(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "massage":
+                                pojo.setMassage(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "password":
+                                pojo.setPassword(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "phoneno":
+                                pojo.setPhoneno(ds.getValue().toString());
+                                break;
+                            case "photography":
+                                pojo.setPhotography(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "type":
+                                pojo.setType(ds.getValue().toString());
+                                break;
+                            case "wax":
+                                pojo.setWax(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                            case "westernbath":
+                                pojo.setWesternbath(Boolean.parseBoolean(ds.getValue().toString()));
+                                break;
+                        }
+                    }
+
+                    if(pojo.isAccepted()) {
+                        saloons_list.add(pojo);
+                    }
+                }
+
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+
+  public void loadMap(View v)
     {
-            startActivity(new Intent(this,
-                    MapsActivity.class));
+
+            String selected_type = "";
+            switch (v.getId()){
+
+                case R.id.ic_makeup:
+                    selected_type = "ic_makeup";
+                    break;
+                case R.id.ic_hairstyle:
+                    selected_type = "ic_hairstyle";
+                    break;
+                case R.id.ic_facial:
+                    selected_type = "ic_facial";
+                    break;
+                case R.id.ic_hair_treatment:
+                    selected_type = "ic_hair_treatment";
+                    break;
+                case R.id.ic_henna:
+                    selected_type = "ic_henna";
+                    break;
+                case R.id.ic_bath:
+                    selected_type = "ic_bath";
+                    break;
+                case R.id.ic_haircut:
+                    selected_type = "ic_haircut";
+                    break;
+                case R.id.ic_wax:
+                    selected_type = "ic_wax";
+                    break;
+                case R.id.ic_eyebrows:
+                    selected_type = "ic_eyebrows";
+                    break;
+                case R.id.ic_massage:
+                    selected_type = "ic_massage";
+                    break;
+                case R.id.ic_hair_protien:
+                    selected_type = "ic_hair_protien";
+                    break;
+                case R.id.ic_photograpgy:
+                    selected_type = "ic_photograpgy";
+                    break;
+
+            }
+            Intent i = new Intent(this, MapsActivity.class);
+            i.putExtra("selected_type",selected_type);
+            startActivity(i);
+
     }
 
 }
--- app/src/main/java/cubex/mahesh/yoursaloon/MapsActivity.java
+++ app/src/main/java/cubex/mahesh/yoursaloon/MapsActivity.java
@@ -11,10 +11,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import cubex.mahesh.yoursaloon.pojos.SaloonPojo;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    String selected_type="";
 
 
     @Override
@@ -22,6 +25,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        selected_type = getIntent().getStringExtra("selected_type");
+
 
         if (android.os.Build.VERSION.SDK_INT > 21) {
             getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.gradient1));
@@ -39,11 +45,260 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+       /* // Add a marker in Sydney and move the camera
         LatLng loc = new LatLng(24.774265, 46.738586);
         mMap.addMarker(new MarkerOptions().position(loc).title("Marker in Saudi"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                loc,10));
+                loc,10));*/
+
+        switch (selected_type) {
+            case "ic_makeup":
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isMakeup()){
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,15));
+                        }
+                    }
+                }
+                break;
+            case "ic_hairstyle":
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isHairstyle()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                           // mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc_obj,17));
+                          //  mMap.animateCamera(CameraUpdateFactory.zoomTo(15f));
+                        }
+
+                    }
+                }
+                break;
+            case "ic_facial":  //bodycare
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isBodycare()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_hair_treatment":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isHairtreatment()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_henna":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isHennadesign()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_bath":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isWesternbath()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+
+                break;
+            case "ic_haircut":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isHaircut()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_wax":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isWax()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_eyebrows":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isEyebrows()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_massage":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isMassage()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_hair_protien":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isHairprotein()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+            case "ic_photograpgy":
+
+                for(SaloonPojo pojo:DashboardActivity.saloons_list){
+                    if(pojo.isPhotography()){
+
+                        String loc = pojo.getLocation();
+                        if(loc.contains(",")){
+                            String[ ] values = loc.split(",");
+                            double lati = Double.parseDouble(values[0]);
+                            double longi = Double.parseDouble(values[1]);
+                            LatLng loc_obj = new LatLng(lati,longi);
+                            mMap.addMarker(new MarkerOptions().position(loc_obj).title(pojo.getEmail()+"\n"+pojo.getPhoneno()));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc_obj));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    loc_obj,10));
+                        }
+
+                    }
+                }
+
+                break;
+        }
     }
 }
